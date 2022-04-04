6c721601806ab53e8cdb41894a8c4be7494bbddc
AydenM01 <aydenwilliammarshall@gmail.com>
2022-04-03 23:41:44 -0400

    Three way

--- src/App.js
+++ src/App.js
@@ -1,58 +1,95 @@
-import { Box, TextField } from "@mui/material";
+import { Box, TextField, Button } from "@mui/material";
 import useSound from "use-sound";
 
-import React, {
-  useEffect,
-  useRef,
-  useState,
-  createRef,
-  VideoHTMLAttributes,
-  useCallback,
-} from "react";
+import React, { useEffect, useState } from "react";
+import { ThemeProvider } from "@emotion/react";
 
 function App() {
-  const [time, setTime] = useState(5);
+  const [startTime, setStartTime] = useState(1);
+  const [waitTime, setWaitTime] = useState(1);
+  const [started, setStarted] = useState(false);
   const [rand, setRand] = useState(0);
 
-  const [one] = useSound("/one.mp3", { volume: 1 });
-  const [two] = useSound("/two.mp3", { volume: 1 });
+  const [go] = useSound("/go.mp3", { volume: 1 });
+  const [stop] = useSound("/stop.mp3", { volume: 1 });
+  const [pause] = useSound("/pause.mp3", { volume: 1 });
+  const [ready] = useSound("/ready.mp3", { volume: 1 });
 
-  const handleChange = (value) => {
-    console.log("changing time to " + value);
-    setTime(value);
+  const handleStartChange = (value) => {
+    console.log("changing start time to " + value);
+    setStartTime(parseInt(value));
+  };
+
+  const handleWaitChange = (value) => {
+    console.log("changing wait time to " + value);
+    setWaitTime(parseInt(value));
+  };
+
+  const handleStart = () => {
+    console.log("starting");
+    setStarted(true);
   };
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      console.log("This will run every time interval!");
-      let newRand = Math.floor(Math.random() * 2);
-      console.log(newRand);
-      console.log(time);
-      if (newRand == 0) {
-        one();
-      } else {
-        two();
-      }
-      setRand(newRand);
-    }, time * 1000);
-    return () => clearInterval(interval);
-  }, [time]);
+    if (started) {
+      const fullInterval = setInterval(() => {
+        console.log("starting repetition");
+        let newRand = Math.floor(Math.random() * 3);
+
+        const timer = setTimeout(() => {
+          console.log("Counting before get Ready");
+          ready();
+        }, startTime * 1000);
+
+        const timer2 = setTimeout(() => {
+          console.log("Counting before g/s/p");
+          if (newRand == 0) {
+            go();
+          } else if (newRand == 1) {
+            stop();
+          } else {
+            pause();
+          }
+          setRand(newRand);
+        }, (startTime + waitTime) * 1000);
+      }, (startTime + waitTime) * 1000);
+
+      return () => clearInterval(fullInterval);
+    }
+  }, [started]);
 
   return (
     <div>
       <TextField
         type="number"
-        label="Wait Time (seconds)"
-        onChange={(e) => handleChange(e.target.value)}
+        label="Start Time (seconds)"
+        onChange={(e) => handleStartChange(e.target.value)}
       ></TextField>
 
-      {rand === 0 ? (
+      <TextField
+        type="number"
+        label="Wait Time (seconds)"
+        onChange={(e) => handleWaitChange(e.target.value)}
+      ></TextField>
+
+      <Button variant="contained" onClick={handleStart}>
+        {" "}
+        Start{" "}
+      </Button>
+
+      {rand === 0 && (
         <Box
-          style={{ backgroundColor: "blue", width: "100vw", height: "100vh" }}
+          style={{ backgroundColor: "green", width: "100vw", height: "100vh" }}
         ></Box>
-      ) : (
+      )}
+      {rand === 1 && (
         <Box
           style={{ backgroundColor: "red", width: "100vw", height: "100vh" }}
+        ></Box>
+      )}
+      {rand === 2 && (
+        <Box
+          style={{ backgroundColor: "yellow", width: "100vw", height: "100vh" }}
         ></Box>
       )}
     </div>
